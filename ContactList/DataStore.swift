0da534a17cf98ecde39b6f616cd3d366549ff467
Alexey Isaev <montilarard@gmail.com>
2022-12-08 21:50:23 +0300

    srart

---
+++ ContactList/DataStore.swift
@@ -0,0 +1,11 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Алексей Исаев on 08.12.2022.
+//
+
+let names = ["Anna", "Jonh", "Jack", "Ted", "Carl"]
+let lastNames = ["Black", "Potter", "Holmes", "Bonde", "Bourne"]
+let emails = ["aa@mail.ru", "bb@mail.ru", "cc@mail.ru", "dd@mail.ru", "ee@mail.ru"]
+let phoneNumbers = ["548278546", "234907582", "303470648", "645390761", "927784521"]
---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Алексей Исаев on 08.12.2022.
+//
+
+struct Person {
+    let name: String
+    let lastName: String
+    let email: String
+    let phoneNumbers: String
+}
