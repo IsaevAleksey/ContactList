455810e3864ab3473ad74f107f615dd82d4af1e2
Alexey Isaev <montilarard@gmail.com>
2022-12-09 21:12:55 +0300

    sections

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -9,5 +9,35 @@ struct Person {
     let name: String
     let lastName: String
     let email: String
-    let phoneNumbers: String
+    let phoneNumber: String
+    var fullName: String {
+        "\(name) \(lastName)"
+    }
+
+    static func getPersonList() -> [Person] {
+        var persons = [Person(name: names.randomElement()!, lastName: lastNames.randomElement()!, email: emails.randomElement()!, phoneNumber: phoneNumbers.randomElement()!)]
+        for person in persons {
+            let newPerson = Person(name: names.randomElement()!, lastName: lastNames.randomElement()!, email: emails.randomElement()!, phoneNumber: phoneNumbers.randomElement()!)
+//            for person in persons {
+//                let newPerson = Person(name: names.randomElement()!, lastName: lastNames.randomElement()!, email: emails.randomElement()!, phoneNumber: phoneNumbers.randomElement()!)
+                if person == newPerson {
+                    continue
+                } else {
+                    persons.append(newPerson)
+
+                }
+            
+        }
+        return persons
+    }
+}
+
+
+extension Person: Equatable {
+    static func ==(lhs: Person, rhs: Person) -> Bool {
+        lhs.name == rhs.name &&
+        lhs.lastName == rhs.lastName &&
+        lhs.email == rhs.email &&
+        lhs.phoneNumber == rhs.phoneNumber
+    }
 }
--- ContactList/ViewControllers/ContactInfoViewController.swift
+++ ContactList/ViewControllers/ContactInfoViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ContactInfoViewController: UIViewController {
-
+    
+    var person: Person!
+    
+    @IBOutlet var phoneLabel: UILabel!
+    
+    @IBOutlet var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = person.fullName
+        phoneLabel.text = person.phoneNumber
+        emailLabel.text = person.email
     }
     
 
